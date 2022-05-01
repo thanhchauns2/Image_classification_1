8b8eb6c7f6c73100367378b3f7b0883da16685f2
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-05-01 19:47:59 +0700

    Update flower_classifier.py

--- flower_classifier.py
+++ flower_classifier.py
@@ -10,7 +10,12 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = tf.keras.utils.get_file(
+    'flower_photos', 
+    origin=dataset_url, 
+    untar=True,
+    cache_dir = "/keras"
+)
 data_dir = pathlib.Path(data_dir)
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
