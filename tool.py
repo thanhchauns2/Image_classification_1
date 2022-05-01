4cafea6fd87f909ef2c69afeca6e567b5407e8ca
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-05-01 15:41:36 +0700

    first CNN code

--- tool.py
+++ tool.py
@@ -1,16 +1,20 @@
+# Importing tensorflow
+
 import tensorflow as tf 
+
+# Generating model
 
 def generate_model():
     model = tf.keras.Sequential([
 
-        tf.keras.layers.Conv2D(32, filter_size = 3, activation = 'relu'),
+        tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape=(32, 32, 3)),
         tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2),
 
-        tf.keras.layers.Conv2D(64, filter_size = 3, activation = 'relu'),
+        tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
         tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2),
 
-        # tf.keras.layers.Conv2D(128, filter_size = 3, activation = 'relu'),
-        # tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation = 'relu'),
+        tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2),
 
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(1024, activation = 'relu'),
@@ -18,5 +22,56 @@ def generate_model():
     ])
     return model
 
+# Summary
+
 model = generate_model()
 model.summary()
+
+# Dataset
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Show dataset
+
+import matplotlib.pyplot as plt
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+# Train model
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+# Plot
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+# Accuracy
+
+print(test_acc)
